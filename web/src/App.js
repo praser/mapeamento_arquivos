258c3d821f66130578aaba60767b85599447c537
Rubens Praser Júnior <praser@gmail.com>
2020-08-26 23:20:20 -0300

    Implementação do filtro dos arquivos

--- web/src/App.js
+++ web/src/App.js
@@ -3,21 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Login, Logout } from './components/session';
 import PrivateRoute from './components/privateRoute';
 import { UserProvider } from './contexts';
-import FileExplorer, { Filter } from './components/fileExplorer';
-import { ApolloProvider, useQuery } from '@apollo/client';
-import client, { LIST_FILES_QUERY } from './services/filesGraphql'
+import FileExplorer from './components/fileExplorer';
+import { ApolloProvider } from '@apollo/client';
+import client from './services/filesGraphql'
 
 const Hello = () => {
-  const { loading, error, data } = useQuery(LIST_FILES_QUERY);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
   return (
     <div>
       <Logout />
-      <Filter placeholder= "Pesquisar..." />
-      <FileExplorer files={data.files} />
+      <FileExplorer />
     </div>
   )
 };
--- web/src/components/fileExplorer/index.js
+++ web/src/components/fileExplorer/index.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client';
+
+import { LIST_FILES_QUERY } from '../../services/filesGraphql';
 import FileList from './fileList';
 import Filter from './filter';
 
-const FileExplorer = ({ fileList, ...rest }) => <FileList files={fileList} {...rest} />
+
+const FileExplorer = () => {
+  const [files, setFiles] = useState([])
+  const { loading, error, data } = useQuery(LIST_FILES_QUERY);
+
+  useEffect(() => {
+    if (data) setFiles(data.files)
+  }, [data])
+
+  const handleFilter = event => {
+    const files = data.files;
+    const query = event.target.value.toLowerCase();
+
+    setFiles(files.filter(file => file.fullName.toLowerCase().includes(query)))
+  }
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div>
+      <Filter placeholder= "Pesquisar..." onChange={handleFilter} />
+      <FileList files={files} />
+    </div>
+  )
+
+}
 
 export default FileExplorer;
-export { Filter };
