b5fd56b19217831cf89360d58f58f6afd87e202e
Rubens Praser Júnior <praser@gmail.com>
2020-10-16 13:06:04 -0300

    Adiciona a paginação dos arquivos no backend

--- backend/src/graphql/resolvers/file.js
+++ backend/src/graphql/resolvers/file.js
@@ -1,16 +1,22 @@
-const { DateTimeResolver } = require("graphql-scalars");
-const { Directory, Checksum, File } = require("../../app/models");
+const { DateTimeResolver } = require("graphql-scalars")
+const { Directory, Checksum, File } = require("../../app/models")
 
 module.exports = {
   DateTime: DateTimeResolver,
   Query: {
-    files: async () =>
-      File.findAll({
+    files: async (_, { page, pageSize }) => {
+      const offset = (page - 1) * pageSize
+      const limit = pageSize
+      console.log(offset, limit)
+      return File.findAll({
+        offset,
+        limit,
         include: [
           { model: Directory, required: true },
           { model: Checksum, required: true },
         ],
-      }),
+      })
+    },
     file: async (_, { id }) => File.findByPk(id),
   },
-};
+}
